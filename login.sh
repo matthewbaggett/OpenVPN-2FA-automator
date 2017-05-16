07ca95364b9c12af999a9ba1bdb50ea946d3b9ca
Matthew Baggett <matthew@baggett.me>
2017-05-16 21:12:31 +0100

    remove 2fa.txt

--- login.sh
+++ login.sh
@@ -4,9 +4,7 @@ echo "Logging into $1 as $2";
 
 qrCodePath="/2fa.jpg";
 qrCodePath=$1$qrCodePath;
-./2fa-key-gen.py -i $qrCodePath > 2fa.txt
-echo -n "2fa code is ";
-cat 2fa.txt;
-./screen-starter.sh $1 $2 $3;
-rm 2fa.txt;
+twofa=`./2fa-key-gen.py -i $qrCodePath`
+echo  "2fa code is $twofa"
+./screen-starter.sh $1 $2 $3 $twofa;
 cd -;
