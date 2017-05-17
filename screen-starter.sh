6c509c803e9eca9e26012ebd186e267edd7097a9
Matthew Baggett <matthew@baggett.me>
2017-05-17 09:02:37 +0100

    Apparently my changes were lost last night

--- screen-starter.sh
+++ screen-starter.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo $@;
 screenName='openvpn-'$1
 clientFile=$1'/client.ovpn'
-echo $screenName
-echo $clientFile;
 
 if ! screen -list | grep -q "$screenName"; then
     screen -d -m -S $screenName sudo openvpn $clientFile;
@@ -11,8 +8,11 @@ else
     screen -S $screenName -p 0 -X sudo openvpn $clientFile"^M"
 fi
 sleep 2;
+echo "Sending Username $2"
 screen -S $screenName -p 0 -X stuff $2"^M"
 sleep 2;
+echo "Sending Password"
 screen -S $screenName -p 0 -X stuff $3"^M"
 sleep 2;
-screen -S $screenName -p 0 -X stuff "`echo 2fa.txt`^M"
+echo "Sending 2FA"
+screen -S $screenName -p 0 -X stuff $4"^M"
